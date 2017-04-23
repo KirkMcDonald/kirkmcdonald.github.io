1f35fd303aeee35774d5192eeb885e90d1b98d3e
Kirk McDonald <kirk@twelve.local>
2017-04-23 02:30:03 -0700

    Add section on multiple unknowns.

--- posts/solve.py
+++ posts/solve.py
@@ -1,13 +1,59 @@
+import itertools
 import sympy
 
 R = sympy.Rational
 
 A = sympy.Matrix([
-    [-4,  0,   3,        1, 0, 0,       1],
-    [ 3, -3,   3,  R(9, 2), 0, 0,       0],
-    [ 0,  2,   4, R(11, 2), 0, 0, R(9, 2)],
-    [-3, -3,   0,       -5, 1, 0,       0],
-    [ 0,  0, -10,      -10, 0, 1,       0],
+    [-4,  0,   3,        1, -2,  0,  0, 0, 0,   0],
+    [ 3, -3,   3,  R(9, 2),  0, -1,  0, 0, 0,   0],
+    [ 0,  2,   4, R(11, 2),  0,  0, -2, 0, 0,   0],
+    [ 0,  0,   0,        0,  1,  1,  1, 0, 0, 100],
+    [-3, -3,   0,       -5,  0,  0,  0, 1, 0,   0],
+    [ 0,  0, -10,      -10,  0,  0,  0, 0, 1,   0],
 ])
+
+def multiples(A):
+    result = set()
+    for i in range(A.shape[0]):
+        indexes = [j for j, x in enumerate(A[i, :-1]) if x > 0]
+        if len(indexes) > 1:
+            result.update(indexes)
+    return sorted(result)
+
+def elide(A, indexes):
+    A = A.copy()
+    for i in sorted(indexes, reverse=True):
+        A.col_del(i)
+    return A
+
+def combinations(A):
+    r = A.shape[1] - A.shape[0] - 1
+    for indexes in itertools.combinations(multiples(A), r):
+        yield elide(A, indexes), set(indexes)
+
+def find_valid(A):
+    for A_prime, indexes in combinations(A):
+        solved, pivots = A_prime.rref()
+        offset = 0
+        x_vector = []
+        for i, x in enumerate(solved[:, -1]):
+            while i + offset in indexes:
+                offset += 1
+                x_vector.append(0)
+            x_vector.append(x)
+        if all(x >= 0 for x in x_vector) and pivots == list(range(len(pivots))):
+            #yield solved, pivots
+            yield x_vector
+
+def cost(x):
+    return x[-1], x[-2]
+
 print(repr(A))
 print(A.rref())
+print()
+solutions = list(find_valid(A))
+for x in solutions:
+    print(" ".join("{:.2f}".format(float(i)) for i in x))
+
+print()
+print(min(solutions, key=cost))
