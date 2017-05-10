b6e1f13a144178c4f9e0253f6cba47d9a1019639
Kirk McDonald <kirklin.mcdonald@gmail.com>
2017-05-10 16:04:08 -0700

    Add centrifuge.

--- factory.js
+++ factory.js
@@ -5,10 +5,11 @@ function Factory(name, speed, modules) {
 }
 
 var CATEGORY_SPEEDS = {
-    chemistry: new Factory("chemical-plant", 1.25, 2),
+    "chemistry": new Factory("chemical-plant", 1.25, 2),
     "oil-processing": new Factory("oil-refinery", 1, 2),
-    smelting: new Factory("furnace", 2, 2),
+    "smelting": new Factory("furnace", 2, 2),
     "rocket-building": new Factory("rocket-silo", 1, 4),
+    "centrifuging": new Factory("centrifuge", 0.75, 2),
 }
 
 var ASSEMBLY_1 = new Factory("assembling-machine", 0.5, 0)
